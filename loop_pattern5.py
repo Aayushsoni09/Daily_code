e26c6326161df0edd2124baf40ff31f714a5ab77
Aayushsoni09 <44709621+Aayushsoni09@users.noreply.github.com>
2022-07-05 14:39:53 +0530

    Add files via upload

---
+++ loop_pattern5.py
@@ -0,0 +1,12 @@
+n=int(input("enter n"))
+i=1
+c=1
+
+while i<=n:
+    j=1
+    while j<=n:
+        print(c,end="")
+        j=j+1
+        c=c+1
+    print("")
+    i=i+1
---
+++ loop_pattern8.py
@@ -0,0 +1,14 @@
+n=int(input("enter n"))
+i=1
+c=1
+
+while i<=n:
+    j=1
+    while j<=i:
+        print(c,end="")
+        j=j+1
+        c=c+1
+    print("")
+    i=i+1
+
+
